eb9f7ba67ef56e9703c766fabfb94c89c5136396
Manaswini1832 <munugurimanaswini@gmail.com>
2025-11-25 23:06:07 +0530

    remove dotenv

--- client/src/lib/helper/supabase.js
+++ client/src/lib/helper/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const supabase = createClient(
     process.env.REACT_APP_SUPABASE_PROJECT_URL, 
